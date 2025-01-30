914338fa4335e65ade70fd9d0f9d51422b2228c7
darshan0604 <darshan06soni@gmail.com>
2025-01-30 22:53:02 +0530

    add lists.py for all lists operations

--- 01_basics/lists.py
+++ 01_basics/lists.py
@@ -6,3 +6,25 @@ print(tea_varities[0:3])
 #Replace String
 tea_varities[0:1] = ['Chochlate']
 print(tea_varities[0:3])
+
+# for tea in tea_varities:
+#     print(tea, end="-")
+
+#removing
+print(tea_varities.pop())
+print(tea_varities.remove("Adrak"))
+
+#inserting
+tea_varities.insert(1, "green") 
+print(tea_varities)
+
+
+# print(tea_varities.pop())
+tea_varities_copy = tea_varities.copy()
+tea_varities_copy.insert(4, "black") 
+
+print(tea_varities_copy)
+
+#amazing
+squared_num = [x**2 for x in range(10)]
+print(squared_num)
