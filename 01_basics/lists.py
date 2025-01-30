7c51c2485d2ee8d0728fbaae52f1371313474d4a
darshan0604 <darshan06soni@gmail.com>
2025-01-30 22:31:50 +0530

    add lists.py for all lists operations

---
+++ 01_basics/lists.py
@@ -0,0 +1,8 @@
+#List
+tea_varities = ['Masala', 'Adrak', 'Lemon']
+print(tea_varities[1])
+print(tea_varities[0:3])
+
+#Replace String
+tea_varities[0:1] = ['Chochlate']
+print(tea_varities[0:3])
--- 01_basics/strings.py
+++ 01_basics/strings.py
@@ -1,12 +1,11 @@
+#indexing
 str = "Darshan soni"
-
 first_char = str[0]
 print(first_char)
 
+#Slicing
 slice = str[0:7]
 print(slice)
-
-#Slicing
 num_list = "0123456789"
 print(num_list[0:8:2])
 print(num_list[8:0:-2])
@@ -19,3 +18,21 @@ print(name.strip())
 chai = "Lemon, Ginger, Masala, Adrak, Choclate"
 print(chai.split()) #default split case
 print(chai.split(", ")) #on paremeter
+
+#find specific word
+chai = "Masala Chai Chai Chai"
+print(chai.find("Chai"))
+print(chai.count("Chai"))
+
+
+#place holders for variables
+message = "Hello {}, I wanna talk with {}"
+print(message.format('Darshan', 'kumari'))
+
+#single letter usig for loop
+for letter in chai:
+    print(letter)
+    
+#quotes issue resolved for using quotes in string use \
+chai = "she said , \"i love coffee then i blocked her\"" 
+print(chai)
