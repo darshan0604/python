d8e8f72cf7c10ca6dd8bc84055c7004936a0048a
darshan0604 <darshan06soni@gmail.com>
2025-01-31 17:34:28 +0530

    if condition for age

--- 02_conditionals/input.py
+++ 02_conditionals/input.py
@@ -1,1 +1,13 @@
-fInput = input("Write Anything -> ")
+# userscore = input("Write Score -> ")
+# print(int(userscore)/20)
+
+# userscore = int(input("Write Score -> "))
+# print(userscore/20)
+
+age = int(input("Write your age -> "))
+
+if age < 13:
+    print("Child")
+elif age <= 19 and age >= 13:
+    print("Teenager")
+    
