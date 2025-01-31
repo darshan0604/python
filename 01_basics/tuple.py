6a3073e3877c15361f7f6eae170236a06f50e0f7
darshan0604 <darshan06soni@gmail.com>
2025-01-31 16:43:48 +0530

    create tuple.py

---
+++ 01_basics/tuple.py
@@ -0,0 +1,17 @@
+tea_types = ("Black", "Adrak", "Masala")
+print(tea_types)
+print(tea_types[1])
+print(tea_types[0:1])
+
+if "Black" in tea_types:
+    print("Yes! Black tea is Available")
+
+print(type(tea_types))
+
+#nesting
+
+nest = (21, (1,2,3), "Darshan")
+print(nest)
+print(nest[1])
+print(nest[1][0:2])
+
