7fdbe35d564d3d4137f1ed6190114e3ab550388b
darshan0604 <darshan06soni@gmail.com>
2025-01-31 18:50:46 +0530

    new conditionals clear

--- 02_conditionals/input.py
+++ 02_conditionals/input.py
@@ -1,17 +1,51 @@
-# userscore = input("Write Score -> ")
-# print(int(userscore)/20)
+# # userscore = input("Write Score -> ")
+# # print(int(userscore)/20)
 
-# userscore = int(input("Write Score -> "))
-# print(userscore/20)
+# # userscore = int(input("Write Score -> "))
+# # print(userscore/20)
 
-age = int(input("Write your age -> "))
+# #Solution 1
+# age = int(input("Write your age -> "))
 
-if age < 13:
-    print("Child")
-elif age <= 19 and age >= 13:
-    print("Teenager")
-elif age <= 60:
-    print("Adult")
-else:
-    print("Senior")
+# if age < 13:
+#     print("Child")
+# elif age <= 19 and age >= 13:
+#     print("Teenager")
+# elif age <= 60:
+#     print("Adult")
+# else:
+#     print("Senior")
     
+# #solution 2
+# #ternary operator
+# price = 12 if age >= 18 else 8
+
+# day = "Wednesday"
+# if day == "Wednesday":
+#     # price = price - 2
+#     price -= 2
+
+# print("Ticket price for you is $", price)
+
+# #Solution 3
+# score = int(input("Enter Percentage -> "))
+
+
+# if(score >= 100 or score <= 0):    
+#     print("Percentage Should be Under 100 and higher 0")
+#     exit()
+    
+# if(score >= 90):
+#     print("A Grade")
+# elif(score >= 70):
+#     print("B Grade")
+# elif(score >= 50):
+#     print("C Grade")
+# elif(score >= 35):
+#     print("D Grade")
+
+#solution4
+fruit = "Mango"
+color = "Orange"
+
+#  sure if fruit = "Mango": 'Yes' else: "No"
