15fbbbe68dc6779127a4113719fd68d745e8817a
darshan0604 <darshan06soni@gmail.com>
2025-02-03 18:21:50 +0530

    update

--- 02_conditionals/input.py
+++ 02_conditionals/input.py
@@ -51,6 +51,11 @@
 #  sure if fruit = "Mango": 'Yes' else: "No"
 #Something
 
-#8solution
+# #leap year
 
-password = "S"
+year = int(input("Type year -> "))
+
+if(year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)):
+    print(year,"Leap Year")
+else:
+    print(year,"Not a leap year")
