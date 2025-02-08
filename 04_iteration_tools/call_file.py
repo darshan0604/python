e60d191a8cbf116fa3d1cd3443bde82d5ca29af9
darshan0604 <darshan06soni@gmail.com>
2025-02-08 16:48:52 +0530

    Call file

---
+++ 04_iteration_tools/call_file.py
@@ -0,0 +1,27 @@
+# f = open('name.py')
+
+# while True:
+#     line = f.readline()
+#     if not line: break
+#     print(line, end='')
+
+# for line in open('name.py'):
+#     print(line, end='')
+    
+# print(f.__next__())
+# print(f.__next__())
+# print(f.__next__())
+# print(f.__next__())
+# print(f.__next__())
+# print(f.__next__())
+# print(f.__next__())
+# print(f.__next__())
+
+# myList = [1,2,3,4]
+# I = iter(myList)
+# print
+
+D = {'a':1, 'b':2}
+
+for key in D:
+    print(key)
