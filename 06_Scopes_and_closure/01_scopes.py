3ef799d06e50cbc258e1eda6fbfd900c6453486d
darshan0604 <darshan06soni@gmail.com>
2025-02-19 17:41:53 +0530

    learn scopes and clouser

--- 06_Scopes_and_closure/01_scopes.py
+++ 06_Scopes_and_closure/01_scopes.py
@@ -1,6 +1,49 @@
-username = "Darshan"
+# username = "Darshan"
 
-def test():
-    print("hello")
+# def test():
+#     # username = "hello"
+#     print(username)
     
-test()
+# test()
+
+# x = 99
+
+# def func(y):
+#     z = x + y
+#     return z
+
+# result = func(1)
+# print(result)
+
+# def func2():
+#     global x
+#     x = 10
+
+# func2()
+# print(x)
+x = 99
+
+# def f1():
+#     x = 88
+#     def f2():
+#         print(x)        
+#     return f2
+ 
+# myResult = f1()
+
+# myResult()
+
+def chaicoder(num):
+    def actual(x):
+        return x ** num
+    return actual
+
+# def chaicoder(2):
+#     def actual(x):
+#         return x ** 2
+#     return actual
+
+f = chaicoder(2)
+# g = chaicoder(3)
+
+print(f)
