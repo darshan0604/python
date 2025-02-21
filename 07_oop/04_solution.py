b9c6ff2b3c010279ea2d8c9726b1242d0564913d
darshan0604 <darshan06soni@gmail.com>
2025-02-21 17:10:03 +0530

    04_solution for inheritance

--- 07_oop/04_solution.py
+++ 07_oop/04_solution.py
@@ -1,14 +1,19 @@
 class Car:
+    total_cars = 0
+    
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
+        Car.total_cars = Car.total_cars + 1
         
     def get_model(self):
-        # self.model = model
         return self.model
     
     def full_name(self):
-        return self.brand + " " + self.model
+        return f"{self.brand}  {self.model}"
+    
+    def fuel_type(self):
+        return "Petrol or Diesel"
 
 class ElectricCar(Car):
     def __init__(self, brand, model, battery_size):
@@ -16,8 +21,17 @@ class ElectricCar(Car):
         self.model = model
         self.battery_size = battery_size
     
+    def fuel_type(self):
+        return "Petrol or Diesel"
+    
 my_tesla = ElectricCar("Tesla", "Truck ", "87KWH")
 print(my_tesla.get_model())
+
+print(my_tesla.fuel_type())
     
-    
-# my_car = Car("Toyota", "Corolla")
+safari = Car("Tata", "Safari")
+print(safari.fuel_type())
+safarithree = Car("Tata", "Safari")
+print(safarithree.fuel_type())
+
+print(Car.total_cars)
