aac054c62906536d79662780211d4102807aac54
darshansonicognisun-droid <darshan.soni.cognisun@gmail.com>
2026-04-08 11:25:42 +0530

    api calls, read files, write files, create visuals

---
+++ 08_imports_modules/02_soluution.py
@@ -0,0 +1,14 @@
+import requests
+
+def get_weather(city):
+    response = requests.get(f"https://wttr.in/{city}?format=j1")    
+    data = response.json()
+    return data['current_condition'][0]
+
+
+city = "New York"
+temperature = get_weather(city)
+temperature
+city = "Ahmedabad"
+temperature = get_weather(city) 
+temperature
